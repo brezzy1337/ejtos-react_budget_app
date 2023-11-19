13f3f610373ab895e98f524d61bce770489b2229
brezzy1337 <90067281+brezzy1337@users.noreply.github.com>
2023-11-19 02:01:42 -0600

    Started Reducer and Intial States

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,1 +1,26 @@
 import React, { createContext, useReducer } from 'react';
+
+export const AppReducer = (state, action) => {
+    let new_budgets = [];
+    switch (action.type) {
+        case 'CHANGE_BUDGET':
+            return {
+                ...state,
+                budgets: [...state.budgets, action.payload]
+            }
+
+        }
+}
+
+
+const intialState = {
+    budget: 0,
+    departmentBudgets: [
+        { id: 1, name: 'Marketing', budget: 200 },
+        { id: 2, name: 'Finance', budget: 100 },
+        { id: 3, name: 'Sales', budget: 50 },
+        { id: 3, name: 'Human Resources', budget: 50 },
+        { id: 3, name: 'Sales', budget: 50 },
+    ],
+    Location: '£'
+}
