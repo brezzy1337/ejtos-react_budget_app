65fb706dcd3adea174cc31e67c20819ad8add2bf
brezzy1337 <90067281+brezzy1337@users.noreply.github.com>
2023-11-19 04:32:41 -0600

    Created Department List UI

---
+++ src/components/DepartmentBudgetList.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import ExpenseItem from './ExpenseItem';
+import { AppContext } from '../context/AppContext';
+
+const departmentBudgetList = () => {
+    const { departmentBudgets } = useContext(AppContext);
+    
+
+    return (
+        <table className='table'>
+              <thead className="thead-light">
+            <tr>
+              <th scope="col">Department</th>
+              <th scope="col">Allocated Budget</th>
+              <th scope="col">Increase by 10</th>
+              <th scope="col">Decrease by 10</th>
+              <th scope="col">Remove</th>
+            </tr>
+          </thead>
+            <tbody>
+            {departmentBudgets.map((departmentBudget) => (
+                <ExpenseItem id={departmentBudget.id} key={departmentBudget.id} name={departmentBudget.name} departmentBudget={departmentBudget.budget} />
+            ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default departmentBudgetList;
