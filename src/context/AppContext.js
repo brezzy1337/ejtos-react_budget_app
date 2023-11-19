da3e648dea8766b7dd125b96ef5527ca3c37197d
brezzy1337 <90067281+brezzy1337@users.noreply.github.com>
2023-11-19 04:06:37 -0600

    Created Budget setting  action type, and passed it to the AppProvider

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,20 +1,22 @@
 import React, { createContext, useReducer } from 'react';
 
 export const AppReducer = (state, action) => {
-    let new_budgets = [];
+    let new_departmentbudgets = [];
     switch (action.type) {
-        case 'CHANGE_BUDGET':
+        case 'SET_BUDGET':
             return {
                 ...state,
-                budgets: [...state.budgets, action.payload]
-            }
-
+                // sets the company budget to the payload sent with the action
+                companyBudget: action.payload
+            };
+            default:
+                return state;
         }
 }
 
 
 const intialState = {
-    budget: 0,
+    companyBudget: 0,
     departmentBudgets: [
         { id: 1, name: 'Marketing', budget: 200 },
         { id: 2, name: 'Finance', budget: 100 },
@@ -22,5 +24,37 @@ const intialState = {
         { id: 3, name: 'Human Resources', budget: 50 },
         { id: 3, name: 'Sales', budget: 50 },
     ],
-    Location: '£'
+    currency: '£'
 }
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+    // 4. Sets up the app state. takes a reducer, and an initial state
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    const setBudget = (newBudget) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newBudget
+        });
+    }
+
+    const totalBudgets = state.expenses.reduce((total, item) => {
+        return (total = total + (item.unitprice*item.quantity));
+    }, 0);
+state.CartValue = totalBudgets;
+    return (
+        <AppContext.Provider
+            value={{
+                CompanyBudget: state.companyBudget,
+                setBudget,
+                DepartmentBudgets: state.departmentBudgets,
+                dispatch,
+                Currency: state.currency
+            }}
+        >
+            {props.children}
+        </AppContext.Provider>
+    );
+};
